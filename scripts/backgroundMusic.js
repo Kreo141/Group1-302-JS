6b08a4afa17a52cedbb5e81ee9eaa90cef8faca2
Cjay Salgado <fionasalgado098@gmail.com>
2024-11-23 21:22:09 +0800

    Refined Starting Point

---
+++ scripts/backgroundMusic.js
@@ -0,0 +1,13 @@
+const audio = document.getElementById('bg-music');
+
+const enableAutoplay = () => {
+  audio.play().then(() => {
+    console.log('Music playing!');
+  }).catch((error) => {
+    console.error('Playback failed:', error);
+  });
+
+  document.removeEventListener('click', enableAutoplay);
+};
+
+document.addEventListener('click', enableAutoplay);
---
+++ scripts/index-hoverEffects.js
@@ -0,0 +1,7 @@
+var container = document.getElementsByClassName("Container")[5];
+
+container.addEventListener("click", expand);
+
+function expand(){
+    this.style.height = "100%";
+}
